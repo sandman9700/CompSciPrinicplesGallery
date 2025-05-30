1ad0089f81a0b3a0b2ebc7478e11a6cef0d43e37
sandman9700 <150147390+sandman9700@users.noreply.github.com>
2025-05-30 15:27:31 -0400

    inital addtions

---
+++ gallery.js
@@ -0,0 +1,39 @@
+$("#display").prop("src", "art/" + worksOfArt[0]);
+
+const worksOfArt = [
+    "asterisk.png", "explosions.png", "GOOP.png", "BURNTHEWITCH.png", "fist.png",
+    "CHEETOWORM.png", "wprm i covered in blue paint.png", "nonsensia.png", "splat.png", "death.png",
+    "theGoat.png", "fish2.png", "theguy.png", "stars.png", "funnybunny.png",
+    "meandbro.png", "hot diggity dawg.png", "crackson grove.png", "coolguymoney.png", "GHUA.png",
+    "fluid.png", "ode.png", "map.png", "uh oh.png", "to the death.png",
+    "quizzzzzz1.png", "cat.png", "hypontize.png", "exsistentialism in a nut.png", "fish.png",
+    "Untitled.png", "ghoul.png", "BURGERDOG.png", "sponge dogg.png", "wacky.png",
+    "nawty.png", "blugh.png", "drip.png", "bloodworm.png", "putitaway.png",
+    "what do you mean.png", "glorb.png", "the guy 2.png", "run.png", "duck.png",
+    "WEEZER.png", "WHOP.png", "bones.png", "drool.png", "reeee.png",
+    "paing.png", "gag.png", "SPAGHETTI.png", "flesh.png", "he he.png",
+    "BLUD.png", "BUK.png", "mouth.png", "slime.png", "stitch.png",
+    "Blursed image.png", "don.png", "crime.png", "fizzy.png", "REEEK.png",
+    "dude.png", "good soup.png", "ow.png", "sus.png", "squish.png",
+    "krungus.png", "fleshies.png", "slopper.png", "hotdog.png", "eyes.png",
+    "spleen.png", "HAAA.png", "splort.png", "mouthfeel.png", "NO.png",
+    "AAAAAAAA.png", "aaa.png", "AYE.png", "shelby.png", "yung blood.png",
+    "teeth.png", "grin.png", "uhhhhh.png", "GRUB.png", "face.png",
+    "blurgh.png", "please.png", "beeg.png", "wheeee.png", "spew.png",
+    "BLEGH.png", "YACK.png", "woah.png", "cavity.png", "screm.png",
+    "scrungle.png", "weird.png", "YIPPEE.png", "burp.png", "plop.png",
+    "ZAMN.png", "carnage.png", "elp.png", "SPILL.png", "yellow.png",
+    "AAAAA.png", "squirt.png", "blort.png", "gunk.png", "seep.png",
+    "gushers.png", "KABOOM.png", "dude2.png", "bloody.png", "HELP.png",
+    "flesh 2.png", "SLORP.png", "guts.png", "BRUH.png", "fuzzy.png",
+    "WTF.png", "gore.png", "barf.png", "AAAAHHHH.png", "WRAAAAAA.png",
+    "SMILE.png", "puke.png", "waz.png", "hollow.png", "stab.png",
+    "ouch.png", "bloopy.png", "eeeeee.png", "num.png", "blep.png",
+    "worm.png", "nope.png", "whyyy.png", "blam.png", "mmm.png",
+    "dawg.png", "maggot.png", "squidge.png", "chomp.png", "bark.png",
+    "why.png", "do not.png", "frrrt.png", "hurgh.png"
+  ];
+  
+  const scalableWidths = new Array(149).fill(1277);
+  const scalableHeights = new Array(149).fill(720);
+  
